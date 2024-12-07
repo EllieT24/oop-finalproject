748ed57c51f1eb3d74b7e528a531f24869380eaf
Ellie Tso <et2227@nyu.edu>
2024-12-07 13:58:21 -0500

    task class actually

--- src/oopfinalproject/Task.java
+++ src/oopfinalproject/Task.java
@@ -1,5 +1,35 @@
 package oopfinalproject;
 
 public class Task {
-
+	static private int counter = 0;
+	private int id;
+	private String name;
+	private boolean complete;
+	
+	public Task(String name) {
+		this.name = name;
+		this.id = counter;
+		this.complete = false;
+		counter++;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public boolean getComplete() {
+		return this.complete;
+	}
+	
+	public void setComplete(boolean c) {
+		this.complete = c;
+	}
 }
